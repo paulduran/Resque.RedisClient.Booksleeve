ca4bdb32b15abc0b0eb63a3febd7adfcc5742051
Paul Duran <paulduran@gmail.com>
2012-10-18 22:25:35 +1100

    modify resque booksleeve client to wait explicitly on the task rather than using the connection.Wait method as per advice from http://code.google.com/p/booksleeve/issues/detail?id=20

--- source/Resque.RedisClient.Booksleeve/RedisBooksleeveConnector.cs
+++ source/Resque.RedisClient.Booksleeve/RedisBooksleeveConnector.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using BookSleeve;
 
 namespace Resque.RedisClient.Booksleeve
@@ -29,19 +30,25 @@ namespace Resque.RedisClient.Booksleeve
 
         public bool SAdd(string key, string redisId)
         {
-            return Client.Wait(Client.Sets.Add(RedisDb, KeyInNamespace(key), redisId));
+            return Wait(Client.Sets.Add(RedisDb, KeyInNamespace(key), redisId));
         }
 
         public string LPop(string key)
         {
-            return Client.Wait(Client.Lists.RemoveFirstString(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.Lists.RemoveFirstString(RedisDb, KeyInNamespace(key)));
+        }
+
+        public T Wait<T>(Task<T> task)
+        {
+            task.Wait();
+            return task.Result;
         }
 
         public Tuple<string,string> BLPop(string[] keys, int timeoutSeconds)
         {
             try
             {
-                return Client.Wait(Client.Lists.BlockingRemoveFirstString(RedisDb, KeyInNamespace(keys), timeoutSeconds));
+                return Wait(Client.Lists.BlockingRemoveFirstString(RedisDb, KeyInNamespace(keys), timeoutSeconds));
             }
             catch(TimeoutException)
             {
@@ -51,43 +58,43 @@ namespace Resque.RedisClient.Booksleeve
 
         public Dictionary<string, string> HGetAll(string key)
         {
-            return Client.Wait(Client.Hashes.GetAll(RedisDb, KeyInNamespace(key))).ToDictionary(k=>k.Key, v=>FromUtf8Bytes(v.Value));
+            return Wait(Client.Hashes.GetAll(RedisDb, KeyInNamespace(key))).ToDictionary(k=>k.Key, v=>FromUtf8Bytes(v.Value));
         }
 
         public string HGet(string key, string field)
         {
-            return Client.Wait(Client.Hashes.GetString(RedisDb, KeyInNamespace(key), field));
+            return Wait(Client.Hashes.GetString(RedisDb, KeyInNamespace(key), field));
         }
 
         public bool HSet(string key, string field, string value)
         {
-            return Client.Wait(Client.Hashes.Set(RedisDb, KeyInNamespace(key), field, value));
+            return Wait(Client.Hashes.Set(RedisDb, KeyInNamespace(key), field, value));
         }
 
         public bool ZAdd(string key, string value, long score)
         {
-            return Client.Wait(Client.SortedSets.Add(RedisDb, KeyInNamespace(key), value, score));
+            return Wait(Client.SortedSets.Add(RedisDb, KeyInNamespace(key), value, score));
         }
 
         public long ZCard(string key)
         {
-            return Client.Wait(Client.SortedSets.GetLength(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.SortedSets.GetLength(RedisDb, KeyInNamespace(key)));
         }
 
         public long ZCard(string key, long min, long max)
         {
-            return Client.Wait(Client.SortedSets.GetLength(RedisDb, KeyInNamespace(key), min, max));
+            return Wait(Client.SortedSets.GetLength(RedisDb, KeyInNamespace(key), min, max));
         }
 
         public Tuple<string, double>[] ZRange(string key, long start, long stop, bool ascending = false)
         {
-            return Client.Wait(Client.SortedSets.RangeString(RedisDb, KeyInNamespace(key), start, stop, ascending))
+            return Wait(Client.SortedSets.RangeString(RedisDb, KeyInNamespace(key), start, stop, ascending))
                 .Select(x=>new Tuple<string, double>(x.Key, x.Value))
                 .ToArray();
         }
         public double ZScore(string key, string member)
         {
-            return Client.Wait(Client.SortedSets.Score(RedisDb, KeyInNamespace(key), member));
+            return Wait(Client.SortedSets.Score(RedisDb, KeyInNamespace(key), member));
         }
 
         private static string FromUtf8Bytes(byte[] bytes)
@@ -97,42 +104,42 @@ namespace Resque.RedisClient.Booksleeve
         
         public long Incr(string key)
         {
-            return Client.Wait(Client.Strings.Increment(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.Strings.Increment(RedisDb, KeyInNamespace(key)));
         }
 
         public IEnumerable<string> SMembers(string key)
         {
-            return Client.Wait(Client.Sets.GetAllString(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.Sets.GetAllString(RedisDb, KeyInNamespace(key)));
         }
 
         public bool Exists(string key)
         {
-            return Client.Wait(Client.Keys.Exists(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.Keys.Exists(RedisDb, KeyInNamespace(key)));
         }
 
         public string Get(string key)
         {
-            return Client.Wait(Client.Strings.GetString(RedisDb, KeyInNamespace(key)));
+            return Wait(Client.Strings.GetString(RedisDb, KeyInNamespace(key)));
         }
 
         public void Set(string key, string value)
         {
-            Client.Wait(Client.Strings.Set(RedisDb, KeyInNamespace(key), value));
+            Client.Strings.Set(RedisDb, KeyInNamespace(key), value).Wait();
         }
 
         public long RemoveKeys(params string[] keys)
         {
-            return Client.Wait(Client.Keys.Remove(RedisDb, KeyInNamespace(keys)));
+            return Wait(Client.Keys.Remove(RedisDb, KeyInNamespace(keys)));
         }
 
         public long SRemove(string key, params string[] values)
         {
-            return Client.Wait(Client.Sets.Remove(RedisDb, KeyInNamespace(key), values));
+            return Wait(Client.Sets.Remove(RedisDb, KeyInNamespace(key), values));
         }
 
         public long RPush(string key, string value)
         {
-            return Client.Wait(Client.Lists.AddLast(RedisDb, KeyInNamespace(key), value));
+            return Wait(Client.Lists.AddLast(RedisDb, KeyInNamespace(key), value));
         }
     }
 }
